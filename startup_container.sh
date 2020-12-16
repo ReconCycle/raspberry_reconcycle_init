4c22d5a9e4bdc468f11d297bdb06323059ca29be
Rok-Pahic <40295241+Rok-Pahic@users.noreply.github.com>
2020-12-16 16:37:24 +0100

    Update startup_container.sh

--- startup_container.sh
+++ startup_container.sh
@@ -1,1 +1,44 @@
+#!/bin/bash
+# chkconfig: 2345 20 80
+# description: Description comes here....
+
+# Source function library.
+. /etc/init.d/functions
+
+start() {
 docker container start ros1_active
+    # code to start app comes here 
+    # example: daemon program_name &
+}
+
+stop() {
+docker container stop ros1_active
+    # code to stop app comes here 
+    # example: killproc program_name
+}
+
+case "$1" in 
+    start)
+       start
+       ;;
+    stop)
+       stop
+       ;;
+    restart)
+       stop
+       start
+       ;;
+    status)
+       # code to check status of app comes here 
+       # example: status program_name
+       ;;
+    *)
+       echo "Usage: $0 {start|stop|status|restart}"
+esac
+
+exit 0 
+
+
+
+
+
