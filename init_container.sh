588c5f4d0bbb42ff61008a8ca034efbaddf438ad
Rok-Pahic <40295241+Rok-Pahic@users.noreply.github.com>
2020-12-16 14:06:41 +0100

    Create init_container.sh

---
+++ init_container.sh
@@ -0,0 +1,14 @@
+
+echo "load and run docker package"
+
+# build the docker image
+git clone https://github.com/ReconCycle/raspi-reconcycle-docker.git
+cd raspi-reconcycle-docker
+docker build -t raspi:active .
+
+
+
+
+# setup automatic start of docker container
+
+docker run -d -v $HOME/reconcycle_config/:/reconcycle_config/ --net=host --restart always --device /dev/mem --privileged --name ros1_active raspi:active
