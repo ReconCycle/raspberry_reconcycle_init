897fd9b9341d396800dc4705f430addd011af632
Rok-Pahic <40295241+Rok-Pahic@users.noreply.github.com>
2021-01-15 09:22:50 +0100

    Update dynamic_startup.sh
    added ros ip in this

--- dynamic_startup.sh
+++ dynamic_startup.sh
@@ -3,7 +3,9 @@ echo "Dynamic startup!"
 source /source_ws.sh
 export ROS_MASTER_URI=$(</reconcycle_config/master_link.txt)
 export THIS_RAS_NAME=$(</reconcycle_config/node_name.txt)
+export ROS_IP=$(</reconcycle_config/ros_ip.txt)
 echo $ROS_MASTER_URI
+echo $ROS_IP
 roslaunch raspi_ros reconcycle.launch 
 
 exec "$@"
